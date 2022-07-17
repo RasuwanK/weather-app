56806511d818a55c6ccc9d06126ae7f50f0b2b4d
RasuwanK <kalharaweragala@gmail.com>
2022-07-18 01:23:36 +0530

    Added JSON data to the UI

--- interfaces/weather-data.ts
+++ interfaces/weather-data.ts
@@ -1,54 +1,55 @@
 type Cordinates = {
-    lon: string;
-    lat: string;
-}
+  lon: string;
+  lat: string;
+};
 
 type Weather = {
-    id: number,
-    main: string,
-    description: string,
-    icon: string;
-}
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
 
 type Main = {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-}
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+};
 
 type Wind = {
-    speed: number;
-    deg: number;
-}
+  speed: number;
+  deg: number;
+  gust: number;
+};
 
 type Cloud = {
-    all: number;
-}
+  all: number;
+};
 
 type Sys = {
-    type: number;
-    id: number;
-    message: number;
-    country: string;
-    sunrise: number;
-    sunset: number;
-}
+  type: number;
+  id: number;
+  message: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+};
 
 export interface WeatherData {
-    cord : Cordinates; 
-    weather: Weather[];
-    base: string;
-    main: Main;
-    visibility: number;
-    wind: Wind;
-    clouds: Cloud;
-    dt: number;
-    sys: Sys;
-    timezone: number;
-    id: number;
-    name: string;
-    cod: number;
+  cord: Cordinates;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Cloud;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
 }
--- pages/forecast.tsx
+++ pages/forecast.tsx
@@ -22,20 +22,27 @@ export default function ForecastPage() {
   // Used to detect live location
   const { location, isLocationLoading, locationError, noLocation } =
     useLocation();
+  // To obtain current weather status
   const { data, isWeatherLoading, weatherError } = useWeather(location);
+
+  // Cached data values
   const [cachedData, setCachedData] = useState<WeatherData>();
+
+  // Theming system based on the current climate
+  const theme = useTheme(data?.weather[0].id || cachedData?.weather[0].id);
 
   useEffect(() => {
     const cache = localStorage.getItem("weather-data");
     setCachedData(JSON.parse(cache!));
   }, [data]);
 
-  //const theme = useTheme(data?.weather[0].id);
+  // In case if geolocation is not awailable in the browser
   if (noLocation) {
     console.error("No geolocation in the browser, internal error !");
     return <p>No geolocation functionality awailable in the browser</p>;
   }
 
+  // In case if location is loading or is not awailable
   if (isLocationLoading) {
     return (
       <Main>
@@ -67,13 +74,15 @@ export default function ForecastPage() {
       <Main>
         <div
           id="forecast-page"
-          style={
-            {
-              //backgroundImage: `linear-gradient(${theme.bg.from}, ${theme.bg.to})`,
-            }
-          }
+          style={{
+            backgroundImage: `linear-gradient(${theme.bg.from}, ${theme.bg.to})`,
+          }}
         >
-          {weatherError && !isWeatherLoading && "Error in weather data"}
+          <div id="status-bar" className="h-10 text-center bg-black text-white">
+            {weatherError &&
+              !isWeatherLoading &&
+              "No internet can't fetch weather data"}
+          </div>
           <CurrentWeather
             main={data?.weather[0].main || cachedData?.weather[0].main}
             description={
@@ -81,6 +90,56 @@ export default function ForecastPage() {
             }
             temperature={data?.main.temp || cachedData?.main.temp}
           />
+          <article className="temp-table grid grid-cols-1">
+            <section className="temp-table-title grid grid-cols-1">
+              Temperature
+            </section>
+            <section className="temp-table-row grid grid-cols-2">
+              <div className="temp-table-col">Minimum Temperature</div>
+              <div className="temp-table-col">
+                {data?.main.temp_min || cachedData?.main.temp_min}
+              </div>
+            </section>
+            <section className="temp-table-row grid grid-cols-2">
+              <div className="temp-table-col">Maximum Temperature</div>
+              <div className="temp-table-col">
+                {data?.main.temp_max || cachedData?.main.temp_max}
+              </div>
+            </section>
+            <section className="temp-table-row grid grid-cols-2">
+              <div className="temp-table-col">Current Temperature</div>
+              <div className="temp-table-col">
+                {data?.main.temp || cachedData?.main.temp}
+              </div>
+            </section>
+            <section className="temp-table-row grid grid-cols-2">
+              <div className="temp-table-col">Feels like</div>
+              <div className="temp-table-col">
+                {data?.main.feels_like || cachedData?.main.feels_like}
+              </div>
+            </section>
+          </article>
+          <article className="wind-table grid grid-cols-1">
+            <section className="wind-table-title">Wind</section>
+            <section className="wind-table-row grid grid-cols-2">
+              <div className="wind-table-col">Wind speed</div>
+              <div className="wind-table-col">
+                {data?.wind.speed || cachedData?.wind.speed}
+              </div>
+            </section>
+            <section className="wind-table-row grid grid-cols-2">
+              <div className="wind-table-col">Wind direction</div>
+              <div className="wind-table-col">
+                {data?.wind.deg || cachedData?.wind.deg}
+              </div>
+            </section>
+            <section className="wind-table-row grid grid-cols-2">
+              <div className="wind-table-col">Gust</div>
+              <div className="wind-table-col">
+                {data?.wind.gust || cachedData?.wind.gust}
+              </div>
+            </section>
+          </article>
         </div>
       </Main>
     );

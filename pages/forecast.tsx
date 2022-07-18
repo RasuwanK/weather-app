da349af78180bc24908fa77c062f7a275ed9f29b
RasuwanK <kalharaweragala@gmail.com>
2022-07-19 00:56:42 +0530

    Changes in the UI (Dashboard left content)

--- pages/forecast.tsx
+++ pages/forecast.tsx
@@ -6,17 +6,8 @@ import { useLocation } from "../hooks/useLocation";
 import { Fragment, useEffect } from "react";
 import { useState } from "react";
 import { WeatherData } from "../interfaces/weather-data";
-
-function Main({ children }: any) {
-  return (
-    <Fragment>
-      <Head>
-        <title>Weather forecast</title>
-      </Head>
-      <Fragment>{children}</Fragment>
-    </Fragment>
-  );
-}
+import Image from "next/image";
+import icon from '../public/weather.svg'
 
 export default function ForecastPage() {
   // Used to detect live location
@@ -43,133 +34,28 @@ export default function ForecastPage() {
     return <p>No geolocation functionality awailable in the browser</p>;
   }
 
-  // In case if location is loading or is not awailable
-  if (isLocationLoading) {
-    return (
-      <Main>
-        <p>Loading location</p>
-      </Main>
-    );
-  } else {
-    if (locationError?.code === 1) {
-      return (
-        <Main>
-          <p>Location permission error</p>
-        </Main>
-      );
-    } else if (locationError?.code === 2) {
-      return (
-        <Main>
-          <p>Location unvailable</p>
-        </Main>
-      );
-    } else if (locationError?.code === 3) {
-      return (
-        <Main>
-          <p>Location timeout error</p>
-        </Main>
-      );
-    }
-
-    return (
-      <Main>
-        <div
-          id="forecast-page"
-          style={{
-            backgroundImage: `url(https://images.unsplash.com/photo-1501630834273-4b5604d2ee31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xvdWR5JTIwc2t5fGVufDB8fDB8fA%3D%3D&w=1000&q=80)`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-          className="w-full pt-28 h-[800px]"
-        >
-          <div className="main-current-container w-full grid grid-cols-1 grid-rows-1">
-            <CurrentWeather
-              main={data?.weather[0].main || cachedData?.weather[0].main}
-              description={
-                data?.weather[0].description ||
-                cachedData?.weather[0].description
-              }
-              temperature={data?.main.temp || cachedData?.main.temp}
-            />
-          </div>
-          <div className="weather-data-container self-center justify-items-center grid md:grid-cols-2 xl:grid-cols-3">
-            <article className="temp-table grid grid-cols-1 p-10 w-[80%]">
-              <section className="temp-table-title grid grid-cols-1 grid-rows-1 justify-items-center">
-                Temperature
-              </section>
-              <section className="temp-table-row grid grid-cols-2 justify-items-center">
-                <div className="temp-table-col">Minimum Temperature</div>
-                <div className="temp-table-col">
-                  {data?.main.temp_min || cachedData?.main.temp_min}
-                </div>
-              </section>
-              <section className="temp-table-row grid grid-cols-2 justify-items-center">
-                <div className="temp-table-col">Maximum Temperature</div>
-                <div className="temp-table-col">
-                  {data?.main.temp_max || cachedData?.main.temp_max}
-                </div>
-              </section>
-              <section className="temp-table-row grid grid-cols-2 justify-items-center">
-                <div className="temp-table-col">Current Temperature</div>
-                <div className="temp-table-col">
-                  {data?.main.temp || cachedData?.main.temp}
-                </div>
-              </section>
-              <section className="temp-table-row grid grid-cols-2 justify-items-center">
-                <div className="temp-table-col">Feels like</div>
-                <div className="temp-table-col">
-                  {data?.main.feels_like || cachedData?.main.feels_like}
-                </div>
-              </section>
-            </article>
-            <article className="wind-table grid grid-cols-1 p-10 w-[80%]">
-              <section className="wind-table-title grid grid-cols-1 grid-rows-1 justify-items-center">
-                Wind
-              </section>
-              <section className="wind-table-row grid grid-cols-2 justify-items-center">
-                <div className="wind-table-col">Wind speed</div>
-                <div className="wind-table-col">
-                  {data?.wind.speed || cachedData?.wind.speed}
-                </div>
-              </section>
-              <section className="wind-table-row grid grid-cols-2 justify-items-center">
-                <div className="wind-table-col">Wind direction</div>
-                <div className="wind-table-col">
-                  {data?.wind.deg || cachedData?.wind.deg}
-                </div>
-              </section>
-              <section className="wind-table-row grid grid-cols-2 justify-items-center">
-                <div className="wind-table-col">Gust</div>
-                <div className="wind-table-col">
-                  {data?.wind.gust || cachedData?.wind.gust}
-                </div>
-              </section>
-            </article>
-            <article className="pressure-table grid-cols-1 p-10 w-[80%]">
-              <section className="pressure-table-title grid grid-cols-1 grid-rows-1 justify-items-center">
-                Pressure
-              </section>
-              <section className="pressure-table-row grid grid-cols-2 justify-items-center">
-                <div className="pressure-table-col">Pressure (sea level)</div>
-                <div className="pressure-table-col">
-                  {data?.main.sea_level || cachedData?.main.sea_level}
-                </div>
-              </section>
-              <section className="pressure-table-row grid grid-cols-2 justify-items-center">
-                <div className="pressure-table-col">Pressure ground level</div>
-                <div className="pressure-table-col">
-                  {data?.main.grnd_level || cachedData?.main.sea_level}
-                </div>
-              </section>
-              <section className="pressure-table-row grid grid-cols-2">
-                <div className="pressure-table-col"></div>
-                <div className="pressure-table-col"></div>
-              </section>
-            </article>
-            <article className="rain-table grid grid-cols-1 w-[80%] justify-items-center"></article>
-          </div>
-        </div>
-      </Main>
-    );
-  }
+  return (
+    <Fragment>
+      <Head>
+        <title>{"Today's"} forecast</title>
+      </Head>
+      <div className="pt-32">
+        <article className="weather-dashboard h-[500px] grid grid-cols-dashboard-lg">
+          <section className="left bg-clear-day bg-cover bg-no-repeat grid grid-cols-1 gap-3 content-center justify-items-center font-open-sans">
+            <div className="weather-icon">
+              <Image alt="weather-icon" src={icon} />
+            </div>
+            <p className="main text-4xl font-bold text-[#736C6C]">
+              {data?.weather[0].main || cachedData?.weather[0].main}
+            </p>
+            <p className="description font-bold text-lg text-[#736C6C]">
+              {data?.weather[0].description ||
+                cachedData?.weather[0].description}
+            </p>
+          </section>
+          <section className="right"></section>
+        </article>
+      </div>
+    </Fragment>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
       fontFamily: {
         "open-sans": ["'Open sans'"],
       },
+      backgroundImage: {
+        "clear-day":
+          "url('https://images.unsplash.com/photo-1558418294-9da149757efe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2400&q=80')",
+        "clear-night":
+          "url('https://images.unsplash.com/photo-1531828051742-b644a99e319d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2400&q=80')",
+      },
+      gridTemplateColumns: {
+        "dashboard-lg": "1fr 2fr",
+      },
     },
   },
   plugins: [],
